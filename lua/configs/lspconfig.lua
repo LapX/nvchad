e871069878b6f87a50d3f93db969acbb431bc478
Tommy Montreuil <tommy.montreuil@lightbeans.com>
2025-04-27 19:57:06 -0400

    eslint

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,4 +1,4 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "ts_ls", "tailwindcss", "eslint" }
+local servers = { "html", "cssls", "ts_ls", "tailwindcss" }
 vim.lsp.enable(servers)
