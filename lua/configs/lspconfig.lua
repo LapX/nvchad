5fd51cece0f8cb6518aa05995b3ec3012991eb23
Tommy Montreuil <tommy.montreuil@lightbeans.com>
2025-05-03 11:16:28 -0400

    added vue

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,4 +1,4 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "ts_ls", "tailwindcss" }
+local servers = { "html", "cssls", "ts_ls", "tailwindcss", "volar" }
 vim.lsp.enable(servers)
