3f8ed1f0047b12b99b96a6f032157d06dba1d0e8
Tommy Montreuil <tommy.montreuil@lightbeans.com>
2025-05-03 11:35:26 -0400

    prettier vue

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -7,13 +7,14 @@ local options = {
     javascriptreact = { "prettier" },
     typescript = { "prettier" },
     typescriptreact = { "prettier" },
+    vue = { "prettier" },
   },
 
-   format_on_save = {
-   -- These options will be passed to conform.format()
-     timeout_ms = 500,
-     lsp_fallback = true,
-   },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
 return options
