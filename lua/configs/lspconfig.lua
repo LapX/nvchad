9056b4c0cc1124c486c5f53ad49c420d28a0e7cd
Tommy Montreuil <tommy.montreuil@lightbeans.com>
2025-05-06 16:26:56 -0400

    vue lsp setup

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -2,3 +2,26 @@ require("nvchad.configs.lspconfig").defaults()
 
 local servers = { "html", "cssls", "ts_ls", "tailwindcss", "volar", "pyright", "svelte" }
 vim.lsp.enable(servers)
+
+local mason_registry = require "mason-registry"
+local vue_language_server = mason_registry.get_package("vue-language-server"):get_install_path()
+  .. "/node_modules/@vue/language-server"
+
+vim.lsp.config("ts_ls", {
+  init_options = {
+    plugins = {
+      {
+        name = "@vue/typescript-plugin",
+        location = vue_language_server,
+        languages = { "javascript", "typescript", "vue" },
+      },
+    },
+  },
+  filetypes = {
+    "javascript",
+    "typescript",
+    "vue",
+    "javascriptreact",
+    "typescriptreact",
+  },
+})
